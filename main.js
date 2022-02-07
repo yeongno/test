e7102aa90424b1b603a95d63b39707130df165f0
rightnow <dldudsh83@gmail.com>
2022-02-08 01:56:11 +0900

    생성자 함수(prototype)

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-//콜백(Callback)
-//함수의 인수로 사용되는 함수
+// const heropy ={
+//   firstName: 'Heropy',
+//   lastName: 'Park',
+//   getFullName: function(){
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(heropy.getFullName())
 
-//setTimeout(함수, 시간)
+// const amy ={
+//   firstName: 'Amy',
+//   lastName: 'Clarke',
+//   getFullName: function(){
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(amy.getFullName())
 
-function timeout(callback){
-  setTimeout(()=>{
-    console.log('Heropy!')
-    callback()
-  },3000)
+// const neo ={
+//   firstName: 'Neo',
+//   lastName: 'Smith',
+//   getFullName: function(){
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(neo.getFullName())
+
+function user(first, last){
+  this.firstName = first
+  this.lastName = last
+  }
+user.prototype.getFullName = function (){
+  return `${this.firstName} ${this.lastName}`
 }
 
-timeout(()=>{
-  console.log('Done!')
-})
+  
+const heropy = new user('Heropy', 'Park')
+const amy = new user('Amy', 'Clarke')
+//new키워드를 통해 생성자 함수로 실행한 결과를 반환해서 할당된 변수인 heropy와 amy는 인스턴스이다.
+
+console.log(heropy.getFullName())
+console.log(amy)
