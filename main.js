7c90e05e60280be54fc808142045d7288edf03ef
rightnow <dldudsh83@gmail.com>
2022-02-07 23:43:47 +0900

    삼항 연산자자

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-// // 산술 연산자(atrithmetic operator)
+//삼항 연산자(ternary operator)
 
-// console.log(1+2)
-// console.log(5-7)
-// console.log(3*4)
-// console.log(10/2)
-// console.log(7%5)
+const a = 1<2
 
-// //할당 연산자(assignment operator)
+if(a){
+  console.log('참')
+}else {
+  console.log('거짓')
+}
 
-// let a = 2
-// // a = a + 1
-// a +=1
-// console.log(a)
-
-// //비교 연산자(Comparison operator)
-
-// const a =1
-// const b = 3
-
-// console.log(a==b)
-
-// function isEqual(x,y){
-//   return x === y
-// }
-
-// console.log(isEqual(1,1))
-// console.log(isEqual(2,'2'))
-// console.log(a!=b)
-// console.log(a<b)
-// console.log(a>=b)
-
-//논리 연산자(logical operator)
-const a = 1===1
-const b = 'AB' === 'AB'
-const c = true
-
-console.log(a)
-console.log(b)
-console.log(c)
-
-console.log('&&: ', a && b && c) //  하나라도 false면 false반환
-console.log('||',a||b||c) //하나라도 true 이면 true 반환
-console.log('!:',!a) //부정 연산자
+console.log(a?'참':'거짓')
