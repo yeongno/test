520b0209d23e5eb70ad2197e5cd2bc506f1dc81f
rightnow <dldudsh83@gmail.com>
2022-02-08 00:26:43 +0900

    변수 유효범위

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-//반복문 (For statement)
-//for(시작조건; 종료조건; 변환조건){}
+//변수 유효범위(Variable Scope)
+// var, let, const
 
-const ulEl = document.querySelector('ul')
-
-for (let i = 0; i < 10; i += 1) {
-  const li = document.createElement('li')
-  li.textContent = `list-${i+1}`
-  if((i+1)%2===0){
-    li.addEventListener('click',function(){
-    console.log(li.textContent)
-  })}
-  ulEl.appendChild(li)
+function scope(){
+  if(true){
+    console.log(a)
+    const a =123
+  }
 }
+scope()
