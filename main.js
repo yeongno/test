930b1184e18af2f8d6660e9db65cffc924a87706
rightnow <dldudsh83@gmail.com>
2022-02-08 01:05:58 +0900

    화살표 함수

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
-//함수 복습
+//화살표 함수
+//() => {} vs function () {}
 
-function sum(x, y) {
-  // console.log(x+y)
-  return x + y
+const double = function (x) {
+  return x*2
 }
-const a = sum(1, 3)
-const b = sum(4, 12)
+console.log('double: ', double(7))
 
-console.log(a)
-console.log(b)
-console.log(a + b)
-console.log(sum(1, 3))
-console.log(sum(1, 3) + sum(4, 5))//효율적이지 않음
-
-sum(1, 3)//함수의 인수로 1,3을 넣으면 함수의 매개변수로 받아 처리
-
-const ssum = function (x,y){
-  console.log(x)
-  return x+y
-  console.log(x)// return에서 종료되기에 아래 값들은 실행되지 않음
-}
-
-console.log(ssum(1,3))
-
-function sssum(){
-  console.log(arguments)
-  return arguments[0] + arguments[1]
-}
-
-console.log(sssum(7,3))
+const doubleArrow = (x) => x*2
+console.log('doubleArrow', doubleArrow(7))
