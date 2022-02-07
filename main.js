a1353109293ecf4e6faa681c9509836e12159d25
rightnow <dldudsh83@gmail.com>
2022-02-08 01:12:26 +0900

    IIFE 즉시실행함수

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-//화살표 함수
-//() => {} vs function () {}
+//즉시실행함수
+//IIFE, Immediately-Invoked Function Expression
 
-const double = function (x) {
-  return x*2
+const a = 7
+function double(){
+  console.log(a*2)
 }
-console.log('double: ', double(7))
+double(); 
+//아래의 즉시실행함수를 실행 시키려면 세미콜론으로 해당 함수가 끝났다는 표시로 구분지어 줘야함.
 
-const doubleArrow = (x) => x*2
-console.log('doubleArrow', doubleArrow(7))
+(function (){
+  console.log(a*2)
+})();
+
+(function (){
+  console.log(a*2)
+}())
