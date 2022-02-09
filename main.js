39e87e20bde8c565bddd6bda6772299c7ec98be4
rightnow <dldudsh83@gmail.com>
2022-02-10 00:06:30 +0900

    수학

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ const float = parseFloat(str)
 console.log(integer)//3 반환
 console.log(float)//3.14 반환
 console.log(typeof integer, typeof float)
+
+console.log('abs: ', Math.abs(-12))//12
+console.log('min: ', Math.min(2,8))//2
+console.log('max: ', Math.max(2,8))//8
+console.log('ceil: ', Math.ceil(3.14))//4 *올림 처리
+console.log('floor: ', Math.floor(3.14))//3 *내림 처리
+console.log('round: ', Math.round(3.14))//3 *반 올림
+console.log('random: ', Math.random())//0.1383785~
+
