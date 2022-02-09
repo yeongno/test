7d237aab6746f9a94df31bb6bbdccb70108668c4
rightnow <dldudsh83@gmail.com>
2022-02-09 23:58:21 +0900

    숫자

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-//JS 데이터
+const pi = 3.141592
+console.log(pi)
 
-//String: "", '', ``
-//Number
-//Boolean: true, false
-//undefined
-//null
-//Array: []
-//Object: {}
+const str = pi.toFixed(2)
+console.log(str)
+console.log(typeof str)
 
-//String.prototype.indexOf()
-const result = 'Hello world'.indexOf('world')//6반환
-console.log(result)
-const str1 ='Hello world!'
-console.log(str1.indexOf('world'))//6반환
-console.log(str1.indexOf('Heropy') !== -1 )//논리 연산자 false 반환
-console.log(str1.slice(0,3))//Hel 반환 *직전까지만 반환
-console.log(str1.replace('world','HEROPY'))//Hello HEROPY 반환
-const email = 'dldudsh123@naver.com'
-console.log(email.match(/.+(?=@)/))
-//['dldudsh123', index: 0, input: 'dldudsh123@naver.com', groups: undefined]
-console.log(email.match(/.+(?=@)/)[0])//dldudsh123만 반환
-//@를 기준으로 정규표현식을 사용 하여 값 반환
-const str2 = '  Hello world  '
-console.log(str2.trim())//Hello world 앞뒤의 공백 문자 생략하여 반환
-
-const str ='0123'
-console.log(str.length)//4반환
-console.log('01234'.length)//5반환
-
-
+//전역 함수를 사용하여 위의 문자 데이터를 숫자 데이터로 바꾸기
+const integer = parseInt(str)
+const float = parseFloat(str)
+console.log(integer)//3 반환
+console.log(float)//3.14 반환
+console.log(typeof integer, typeof float)
